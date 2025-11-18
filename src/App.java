a011cd285a2d2eb9fe30baefa7475a921991ce23
Sebastian Lopez <2006sacura@gmail.com>
2025-11-18 07:57:05 -0500

    Practica Busqueda

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import controllers.MetodoBinario;
+import models.Persona;
+import views.showConsole;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+           showConsole mostrar = new showConsole();
+        Persona[] personas = MetodoBinario.ingresarPersonas(mostrar);
+        MetodoBinario mb = new MetodoBinario( personas);
+
+        MetodoBinario.ordenamiento(personas);
+        mostrar.showMessage("Arreglo ordenado:");
+
+        mb.printArray(personas);
+
+        int ageToFind = mostrar.inputAge();
+        mb.showPersonByAge(ageToFind);
+        
+    }
+}
+
